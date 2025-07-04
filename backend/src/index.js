c22454a8313f942f992c522fcc9a99b744a40733
Santosh Saroj <santoshsaroj0032@gmail.com>
2025-07-04 19:00:13 +0530

    .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
